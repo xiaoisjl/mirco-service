eccf027c9d8eded2ac22ba67085025a6c1b7a311
ucs_liwenhui <240511516@qq.com>
2019-05-13 09:54:21 +0800

    添加config，修改user模块命名

---
+++ config/src/main/java/inid/hui/booking/config/ConfigApplication.java
@@ -0,0 +1,19 @@
+package inid.hui.booking.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author ucs_liwenhui
+ * @description TODO
+ * @date 2019/5/13 9:45
+ **/
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
